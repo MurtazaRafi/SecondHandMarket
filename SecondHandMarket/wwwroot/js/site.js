511b51944c482445a3d45b22c5b7e6c1806c7a84
Murtaza Rafi <murtaza_bayat@hotmail.com>
2021-10-04 12:31:53 +0200

    Almost working

--- SecondHandMarket/wwwroot/js/site.js
+++ SecondHandMarket/wwwroot/js/site.js
@@ -21,13 +21,24 @@ $(function () {
 function GetSubLocations(lId) {
     $("select#sublocations").empty().append('<option value="0" disabled="disabled" , selected="selected">Välj kommun</option>');
     $("select#sublocations").find("option").first().hide();
-    //TODO fixa subcetegory controller och ha metoden där (kalla på den)
+    //TODO fixa subcategory controller och ha metoden där (kalla på den)
     $.getJSON(`/Advertisements/GetSubLocations?lId=${lId}`, function (data) {
         $.each(data, function (i, item) {
             $("select#sublocations").append(`<option value="${item.id}">${item.name}</option>`);
         });
     });
 }
+
+
+
+$(function () {
+    let uploadBtn = document.getElementsByClassName('upload-Btn');
+    $(uploadBtn).click(function (e) {
+        e.preventDefault();
+        $('#fileId1').click();
+    }
+    );
+});
 
 
 fileId1.onchange = evt => {
@@ -40,53 +51,51 @@ fileId1.onchange = evt => {
     }
 }
 
-$(function () {
-    let uploadBtn = document.getElementsByClassName('upload-Btn');
-    $(uploadBtn).click(function (e) {
-        e.preventDefault();
-        $('#fileId1').click();
-    }
-    );
-});
+//$(function () {
+//    let deleteButton = document.getElementById('deleteButton');
+//    deleteButton.onclick = function () {
+//        //document.getElementById("fileId1").value = null;
+//        //document.getElementById("fileId1").remove();
+//        //document.getElementById("fileId1").setAttribute("id", "");
+
+//        if (document.getElementById("fileId2").value != "") {
+//            document.getElementById("fileId2").setAttribute("id", "fileId1");
+
+//            //document.getElementById("image2").setAttribute("id", "image1");
+//            document.getElementById("div1").remove();
+//            document.getElementById("div2").setAttribute("id", "div1");
+//            //var img2 = $("#div2").children('img').eq(0);
+//            //img2.setAttribute("id", "image1");
+
+//        }
+//        else {
+
+//            image1.src = "/Pics/kamera.PNG";
+//        }
+//        //TODO lägg till If sats (om fil 2 null visa kamera annars ej)
+//    };
+//});
 
 
-$(function () {
-    let deleteButton = document.getElementById('deleteButton');
-    deleteButton.onclick = function () {
-        document.getElementById("fileId1").value = null;
+//fileId2.onchange = evt => {
+//    var images = [image1, image2, image3, image4, image5, image6];
+//    var files = [fileId1, fileId2];
+//    var file = (files[0])[0].files;
 
-        if (document.getElementById("fileId2").value != null) {
-            document.getElementById("fileId2").setAttribute("id", "fileId1");
+//        image2.src = URL.createObjectURL(file[0])
+//}
 
-        }
-        else {
-
-        image1.src = "/Pics/kamera.PNG";
-        }
-        //TODO lägg till If sats (om fil 2 null visa kamera annars ej)
-    };
-});
+var loadFile = function (event) {
+    var output = document.getElementById('image2');
+    output.src = URL.createObjectURL(event.target.files[0]);
+    $("#close").show();
+};
 
 
-fileId2.onchange = evt => {
-    var images = [image1, image2, image3, image4, image5, image6];
-    var files = $("#fileId2")[0].files;
-
-    for (var i = 0; i < files.length; i++) {
-
-        image2.src = URL.createObjectURL(files[i])
-    }
-}
-
-// Experimentrar kan tas bort sen
-//var loadFile = function (event) {
-//    var output = document.getElementById('output');
-//    output.src = URL.createObjectURL(event.target.files[0]);
-//    $("#clear").removeClass("hide");
-//};
-
-
-
+$('#deleteButton').click(function () {
+    var div = $(this).parent();
+    div.remove();
+})
 
 //(function () {
 //    let deleteButton = document.getElementById('deleteButton');
